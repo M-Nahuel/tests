43b04ca1a19eac0eca669b56597335c9186be5db
M_Nahuel <91757112+M-Nahuel@users.noreply.github.com>
2023-04-25 22:21:39 -0400

    Counter for each character added

--- Desktop/Coding/py/tests/latincounter.py
+++ Desktop/Coding/py/tests/latincounter.py
@@ -5,8 +5,16 @@ def islatin(ch):
         return False
     return True
 
+dictionary = {}
 
-inp = input('Ingrese el nombre del archivo que desea inspeccionar.')
+def list(ch):
+    if ch not in dictionary:
+        dictionary[ch] = 1
+    else:
+        dictionary[ch] = dictionary[ch]+1
+
+
+inp = input('Ingrese el nombre del archivo que desea inspeccionar: ')
 try:
     src = open(inp, 'rt')
 except IOError as e:
@@ -21,11 +29,14 @@ try:
         for line in lines:
             for ch in line:
                 if islatin(ch):
+                    list(ch)
                     count += 1
 except IOError as e:
     print('No se pudo terminar el proceso.')
     exit(e.error)
 
-print(count, 'Letras latinas contadas.')
+print(count, ' Letras latinas contadas.')
+print('Cantidad de cada letra: ')
+for i in dictionary:
+    print(i + ' -> ' + str(dictionary[i]))
 src.close()
-
